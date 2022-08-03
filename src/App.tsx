4a7e53714af1e5c9158d3684f0959985bf191345
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 15:18:53 +0900

    ADD - layout for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 import JoinScreen from './screen/JoinScreen';
 import LoginScreen from './screen/LoginScreen';
 import { routes } from './screen/routes';
 
+const Wrapper = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={routes.home} element={<LoginScreen />} />
-        <Route path={routes.join} element={<JoinScreen />} />
-      </Routes>
-    </Router>
+    <Wrapper>
+      <Router>
+        <Routes>
+          <Route path={routes.home} element={<LoginScreen />} />
+          <Route path={routes.join} element={<JoinScreen />} />
+        </Routes>
+      </Router>
+    </Wrapper>
   );
 }
 
--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.form`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  form {
+    border: 2px solid gray;
+    border-radius: 20px;
+    width: 400px;
+    height: 700px;
+    display: flex;
+    flex-direction: column;
+    justify-content:space-around;
+    align-items: center;
+    padding: 20px;
+  }
+`;
 
 function LoginScreen() {
-  return <>Login</>;
+  return (
+    <Wrapper>
+      <form>
+        <span className="text-head">로그인</span>
+        <input className="input" data-cy="input-email" placeholder="이메일을 입력해주세요." />
+        <span>비밀번호</span>
+        <input className="input" data-cy="input-password" placeholder="비밀번호를 입력해주세요." />
+        <button className="btn-login" type="submit" data-cy="button-login">
+          로그인
+        </button>
+        <a className="link-join" href="/join" data-cy="link-join">
+          회원가입하기
+        </a>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default LoginScreen;
