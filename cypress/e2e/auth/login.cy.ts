a6926a9f7e61759e5f57c718c0bf397c97709615
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 15:26:53 +0900

    ADD - e2e test case for link to join

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -1,4 +1,5 @@
 import { buttonLogin, inputEmail, inputPassword, linkJoin } from "../data-tags"
+import {routes} from '../../../src/screen/routes'
 
 describe('login page', () => {
   it('should visit login page', () => {
@@ -6,9 +7,18 @@ describe('login page', () => {
   })
 
   it('should render inputs and buttons', () => {
+    cy.visit("/")
+
     cy.get(inputEmail).should('exist')
     cy.get(inputPassword).should('exist')
     cy.get(buttonLogin).should('exist')
     cy.get(linkJoin).should('exist')
+  })
+
+  it('should link to join', () => {
+    cy.visit("/")
+
+    cy.get(linkJoin).should('exist').should('have.attr','href',routes.join).click()
+    cy.url().should('eq', Cypress.config().baseUrl + routes.join)
   })
 })
