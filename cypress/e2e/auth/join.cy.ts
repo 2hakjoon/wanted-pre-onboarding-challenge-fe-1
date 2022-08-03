4dbc3a204228f9dca867db7d13f27d363f81e6ec
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 12:42:34 +0900

    ADD - login, auth basic e2e test

---
+++ cypress/e2e/auth/join.cy.ts
@@ -0,0 +1,14 @@
+import { buttonJoin, inputEmail, inputPassword, linkLogin } from "../data-tags"
+
+describe('join page', ()=>{
+  it('should visit join page', () =>{
+    cy.visit("/join")
+  })
+
+  it('should render inputs and buttons', () => {
+    cy.get(inputEmail).should('exist')
+    cy.get(inputPassword).should('exist')
+    cy.get(buttonJoin).should('exsis')
+    cy.get(linkLogin).should('exist')
+  })
+})
---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,15 @@
+import { buttonLogin, inputEmail, inputPassword, linkJoin } from "../data-tags"
+
+describe('login page', ()=>{
+  it('should visit login page', () =>{
+    cy.visit("/login")
+  })
+
+  
+  it('should render inputs and buttons', () => {
+    cy.get(inputEmail).should('exist')
+    cy.get(inputPassword).should('exist')
+    cy.get(buttonLogin).should('exsis')
+    cy.get(linkJoin).should('exist')
+  })
+})
