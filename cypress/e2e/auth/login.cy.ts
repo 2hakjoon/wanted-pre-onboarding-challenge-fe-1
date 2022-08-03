fe33ee8e9caa42f965ef74b34f811b332613fd86
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 14:54:39 +0900

    FIX - login e2e test case typo

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -2,13 +2,13 @@ import { buttonLogin, inputEmail, inputPassword, linkJoin } from "../data-tags"
 
 describe('login page', () => {
   it('should visit login page', () => {
-    cy.visit("/login")
+    cy.visit("/")
   })
 
   it('should render inputs and buttons', () => {
     cy.get(inputEmail).should('exist')
     cy.get(inputPassword).should('exist')
-    cy.get(buttonLogin).should('exsis')
+    cy.get(buttonLogin).should('exist')
     cy.get(linkJoin).should('exist')
   })
 })
