472310225e7fea2bffd6b63cecf78070d1f7902a
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 12:27:56 +0900

    ADD - data-cy tags

---
+++ cypress/e2e/data-tags.cy.ts
@@ -0,0 +1,7 @@
+export const inputEmail = '[data-cy="input-email"]'
+export const inputPassword = '[data-cy="input-password"]'
+export const buttonLogin = '[data-cy="button-login"]'
+export const buttonJoin = '[data-cy="button-join"]'
+export const linkLogin = '[data-cy="link-login"]'
+export const linkJoin = '[data-cy="link-join"]'
+
