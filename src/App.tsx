8caa0692c90c6469cb3972ed0fd076b883825a53
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-03 13:29:06 +0900

    ADD - login, logout routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import JoinScreen from './screen/JoinScreen';
+import LoginScreen from './screen/LoginScreen';
+import { routes } from './screen/routes';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path={routes.home} element={<LoginScreen />} />
+        <Route path={routes.join} element={<JoinScreen />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/screen/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function LoginScreen() {
+  return <>Login</>;
+}
+
+export default LoginScreen;
