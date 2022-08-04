c3500b2e31a0204cab7f12c7c3da36066542c74b
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-04 21:10:09 +0900

    FIX - typo in config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3001',
     env:{
-      authToken :"eyJhbGciOiJIUzI1NiJ9.YXNkZkBhc2RmYXNkZi5jb20.h-oLZnV0pCeNKa_AM3ilQzerD2Uj7bKUn1xDft5DzOk"
+      authToken : "eyJhbGciOiJIUzI1NiJ9.YXNkZkBhc2RmYXNkZi5jb20.h-oLZnV0pCeNKa_AM3ilQzerD2Uj7bKUn1xDft5DzOk",
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
