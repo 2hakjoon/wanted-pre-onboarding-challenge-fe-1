13c5027301bb39b562e3c8e0691d43155b6ad17b
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-04 15:07:11 +0900

    IMPLEMENT - login mutation

---
+++ src/api/Auth/login.ts
@@ -0,0 +1,22 @@
+import { apiEndpont } from '../endpoints';
+
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  message?: string;
+  token?: string;
+  details?: string;
+}
+
+export const apiLogin = async (body: LoginParams): Promise<LoginResponse> => {
+  return fetch(apiEndpont.login, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json());
+};
--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,7 +1,13 @@
+import { useMutation } from '@tanstack/react-query';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { apiLogin, LoginResponse } from '../api/Auth/login';
+import { apiSignUp } from '../api/Auth/signUp';
+import { emailPattern, passwordPattern } from '../common/constants/regex';
 
-const Wrapper = styled.form`
+const Wrapper = styled.section`
   display: flex;
   width: 100%;
   height: 100vh;
@@ -14,21 +20,68 @@ const Wrapper = styled.form`
     height: 700px;
     display: flex;
     flex-direction: column;
-    justify-content:space-around;
+    justify-content: space-around;
     align-items: center;
     padding: 20px;
   }
 `;
 
 function LoginScreen() {
+  const { register, getValues, formState } = useForm({ mode: 'onChange' });
+  const navigate = useNavigate();
+
+  const { data, mutate } = useMutation(apiLogin);
+
+  const loginHandler = (e: any) => {
+    e.preventDefault();
+
+    const onSuccess = ({ message, details, token }: LoginResponse) => {
+      if (details) {
+        window.alert(details);
+        return;
+      }
+      if (token) {
+        window.alert('로그인이 완료되었습니다.');
+        localStorage.setItem('TOKEN', token);
+        window.location.reload();
+      }
+    };
+
+    mutate({ email: getValues('email'), password: getValues('password') }, { onSuccess });
+  };
+
+  const isFullFilled = () => {
+    return (
+      Boolean(formState.errors.email?.type) === true ||
+      Boolean(formState.errors.password?.type) === true ||
+      !Object.keys(getValues()).length
+    );
+  };
+
   return (
     <Wrapper>
       <form>
         <span className="text-head">로그인</span>
-        <input className="input" data-cy="input-email" placeholder="이메일을 입력해주세요." />
+        <input
+          {...register('email', { pattern: emailPattern })}
+          className="input"
+          data-cy="input-email"
+          placeholder="이메일을 입력해주세요."
+        />
         <span>비밀번호</span>
-        <input className="input" data-cy="input-password" placeholder="비밀번호를 입력해주세요." />
-        <button className="btn-login" type="submit" data-cy="button-login">
+        <input
+          {...register('password', { pattern: passwordPattern })}
+          className="input"
+          data-cy="input-password"
+          placeholder="비밀번호를 입력해주세요."
+        />
+        <button
+          className="btn-login"
+          type="submit"
+          data-cy="button-login"
+          onClick={(e) => loginHandler(e)}
+          disabled={isFullFilled()}
+        >
           로그인
         </button>
         <a className="link-join" href="/join" data-cy="link-join">
