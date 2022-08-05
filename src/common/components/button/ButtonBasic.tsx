e60d744929083d87f6c2841533411dff2f204d56
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-05 16:38:29 +0900

    ADD - ButtonBasic component

---
+++ src/common/components/button/ButtonBasic.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({theme})=>theme.colors.blue};
+  border: none;
+  border-radius: 20px;
+  padding: 10px 20px;
+  color: white;
+  cursor: pointer;
+`;
+
+interface ButtonBasicProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+}
+
+function ButtonBasic({ title, ...rest }: ButtonBasicProps) {
+  return <Button {...rest}>{title}</Button>;
+}
+
+export default ButtonBasic;
