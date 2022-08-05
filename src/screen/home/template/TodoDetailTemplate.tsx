9d0c338a3f337eecd0bf83fcd3c3f095dd210271
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-05 23:18:11 +0900

    STYLE - change button to ButtonBasic and css layout

--- src/screen/home/template/TodoDetailTemplate.tsx
+++ src/screen/home/template/TodoDetailTemplate.tsx
@@ -3,20 +3,32 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { apiGetTodoById, ApiGetTodoById, apiGetTodos, ApiGetTodosResponse, apiUpdateTodo } from '../../../api/Todos/todos';
+import dayjs from 'dayjs';
+import {
+  apiGetTodoById,
+  ApiGetTodoById,
+  apiGetTodos,
+  ApiGetTodosResponse,
+  apiUpdateTodo,
+} from '../../../api/Todos/todos';
 import { TodoParams } from '../../../api/Todos/types';
+import ButtonBasic from '../../../common/components/button/ButtonBasic';
 
 const Wrapper = styled.article`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  width: 100%;
+  border: 2px solid darkgray;
+  border-radius: 30px;
+  padding: 20px 20px;
+  .button-edit-mode {
+    align-self: flex-end;
+  }
 `;
 
 function TodoDetailTemplate() {
   const { id: todoId } = useParams();
-  if (!todoId) {
-    return <>Todo를 선택해주세요.</>;
-  }
 
   const { refetch: refetchTodos } = useQuery<ApiGetTodosResponse>(['getTodos'], apiGetTodos);
   const { register, handleSubmit } = useForm<TodoParams>();
@@ -37,6 +49,9 @@ function TodoDetailTemplate() {
   };
 
   const handleUpdateTodo = ({ title, content }: TodoParams) => {
+    if (!todoId) {
+      return;
+    }
     if (title.length === 0 || content.length === 0) {
       window.alert('내용을 입력해주세요.');
       return;
@@ -51,29 +66,35 @@ function TodoDetailTemplate() {
 
   return (
     <Wrapper>
-      {todoData && (
+      {todoData ? (
         <>
           {!editMode ? (
-            <button type="button" data-cy="button-edit-mode" onClick={onEditMode}>
-              수정
-            </button>
+            <ButtonBasic
+              className="button-edit-mode"
+              title="수정"
+              type="button"
+              data-cy="button-edit-mode"
+              onClick={onEditMode}
+            />
           ) : (
             <form onSubmit={handleSubmit(handleUpdateTodo)}>
               <input {...register('title')} placeholder={todoData.title} data-cy="input-edit-todo-title" />
               <input {...register('content')} placeholder={todoData.content} data-cy="input-edit-todo-content" />
-              <button type="button" data-cy="button-edit-cancel" onClick={closeEditMode}>
-                취소
-              </button>
-              <button type="submit" data-cy="button-edit-save">
-                저장
-              </button>
+              <ButtonBasic title="취소" type="button" data-cy="button-edit-cancel" onClick={closeEditMode} />
+              <ButtonBasic title="저장" type="submit" data-cy="button-edit-save" />
             </form>
           )}
-          <span data-cy="text-todo-detail-title">{todoData.title}</span>
-          <span data-cy="text-todo-detail-content">{todoData.content}</span>
-          <span data-cy="text-todo-detail-createdAt">{todoData.createdAt}</span>
-          <span data-cy="text-todo-detail-updatedAt">{todoData.updatedAt}</span>
+          <span data-cy="text-todo-detail-title">제목 : {todoData.title}</span>
+          <span data-cy="text-todo-detail-content">내용 : {todoData.content}</span>
+          <span data-cy="text-todo-detail-createdAt">{`작성한 시간 : ${dayjs(todoData.createdAt).format(
+            'YYYY/YY/MM - HH시mm분',
+          )}`}</span>
+          <span data-cy="text-todo-detail-updatedAt">{`마지막으로 수정한 시간 : ${dayjs(todoData.updatedAt).format(
+            'YYYY/YY/MM - HH시mm분',
+          )}`}</span>
         </>
+      ) : (
+        <span>할 일을 선택해주세요.</span>
       )}
     </Wrapper>
   );
