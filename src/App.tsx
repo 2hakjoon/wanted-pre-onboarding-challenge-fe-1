09570dba7eaa777c8ef7c8ba48bd6ebc037891f5
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-05 16:38:09 +0900

    ADD - themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import SignUpScreen from './screen/sign-up/SignUpScreen';
 import LoginScreen from './screen/login/LoginScreen';
 import { routes } from './screen/routes';
 import { authToken } from './common/constants/local-storage';
 import HomeScreen from './screen/home/HomeScreen';
+import { theme } from './style/theme';
 
 const Wrapper = styled.main`
   width: 100%;
@@ -19,21 +20,23 @@ function App() {
 
   return (
     <Wrapper>
-      <Router>
-        <Routes>
-          {isLoggedIn ? (
-            <>
-              <Route path={routes.home} element={<LoginScreen />} />
-              <Route path={routes.join} element={<SignUpScreen />} />
-            </>
-          ) : (
-            <>
-              <Route path={routes.home} element={<HomeScreen />} />
-              <Route path={routes.todo} element={<HomeScreen />} />
-            </>
-          )}
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            {isLoggedIn ? (
+              <>
+                <Route path={routes.home} element={<LoginScreen />} />
+                <Route path={routes.join} element={<SignUpScreen />} />
+              </>
+            ) : (
+              <>
+                <Route path={routes.home} element={<HomeScreen />} />
+                <Route path={routes.todo} element={<HomeScreen />} />
+              </>
+            )}
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </Wrapper>
   );
 }
---
+++ src/style/theme.ts
@@ -0,0 +1,8 @@
+const colors = {
+  blue: '#3065AC',
+};
+export type Color = typeof colors;
+
+export const theme = {
+    colors,
+};
