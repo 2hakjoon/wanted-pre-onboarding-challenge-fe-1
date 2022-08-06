8c6c12b9e5f0d02ff12492d169558f28e0a32b2c
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-06 09:51:40 +0900

    FIX - when logout, move to root

--- src/screen/home/component/ButtonLogOut.tsx
+++ src/screen/home/component/ButtonLogOut.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import ButtonBasic from '../../../common/components/button/ButtonBasic';
 import { authToken } from '../../../common/constants/local-storage';
 
 function ButtonLogOut() {
+  const navigate = useNavigate()
   const logoutHandler = () => {
     localStorage.removeItem(authToken);
+    navigate("/")
     window.location.reload();
   };
 
