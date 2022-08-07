d7ec327ff1efc170712bf67ab744a81cb4ef4761
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-08 00:34:10 +0900

    REFACTOR - apiFetch with Middlewares

--- src/api/custom-fetch.ts
+++ src/api/custom-fetch.ts
@@ -2,6 +2,27 @@ import { authToken } from '../common/constants/local-storage';
 
 export type FetchResponse<T> = { data: T };
 
+const tokenNotValid = 'Token is missing';
+
+interface apiResponse extends Response {
+  details: string;
+}
+
+const errorMiddleware = async (data: apiResponse) => {
+  if (data.details === tokenNotValid) {
+    window.alert('다시 로그인을 진행해주세요.');
+    window.location.reload();
+    return data;
+  }
+  return data;
+};
+
+const apiFetchMiddlewares = async (res: Response) => {
+  let data = await res.json();
+  data = errorMiddleware(data);
+  return data;
+};
+
 export const apiFetch = {
   get: <T>(url: string): Promise<T> =>
     fetch(url, {
@@ -10,7 +31,8 @@ export const apiFetch = {
         'Content-Type': 'application/json',
         Authorization: localStorage.getItem(authToken) || '',
       },
-    }).then((res) => res.json()),
+    }).then(apiFetchMiddlewares),
+
   post: <T>(url: string, body: object): Promise<T> =>
     fetch(url, {
       method: 'POST',
@@ -19,7 +41,8 @@ export const apiFetch = {
         Authorization: localStorage.getItem(authToken) || '',
       },
       body: JSON.stringify(body),
-    }).then((res) => res.json()),
+    }).then(apiFetchMiddlewares),
+
   delete: <T>(url: string): Promise<T> =>
     fetch(url, {
       method: 'DELETE',
@@ -27,7 +50,8 @@ export const apiFetch = {
         'Content-Type': 'application/json',
         Authorization: localStorage.getItem(authToken) || '',
       },
-    }).then((res) => res.json()),
+    }).then(apiFetchMiddlewares),
+
   put: <T>(url: string, body: object): Promise<T> =>
     fetch(url, {
       method: 'PUT',
@@ -36,5 +60,5 @@ export const apiFetch = {
         Authorization: localStorage.getItem(authToken) || '',
       },
       body: JSON.stringify(body),
-    }).then((res) => res.json()),
+    }).then(apiFetchMiddlewares),
 };
