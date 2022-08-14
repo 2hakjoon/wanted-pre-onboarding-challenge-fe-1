d5491486085b49b8c7dcf8426073eadea6fa46b7
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-14 13:59:39 +0900

    ADD - contextApi for HeduxStore.Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import createHeduxStore from './hedux/core/hedux';
+import reducer from './hedux/reducer/reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
-      retry:0,
-      useErrorBoundary:true,
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      useErrorBoundary: true,
     },
-    mutations:{
-      useErrorBoundary:true,
-    }
-  }
+    mutations: {
+      useErrorBoundary: true,
+    },
+  },
 });
+
+const heduxStore = createHeduxStore({ reducer });
+export const HeduxStore = createContext(heduxStore);
 
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <HeduxStore.Provider value={heduxStore}>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </HeduxStore.Provider>
   </React.StrictMode>,
 );
