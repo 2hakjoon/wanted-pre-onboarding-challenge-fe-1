3a2fea56d65c2917f238a8e591cbe178b08c6a10
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-15 21:05:13 +0900

    ADD - axios intercepteros. request and response

--- src/api/custom-fetch.ts
+++ src/api/custom-fetch.ts
@@ -1,13 +1,50 @@
-import axios from 'axios';
+/* eslint-disable consistent-return */
+/* eslint-disable no-param-reassign */
+import axios, { AxiosError } from 'axios';
 import { authToken } from '../common/constants/local-storage';
 import { backendBaseUrl } from './endpoints';
 
 export type FetchResponse<T> = { data: T };
 
+export interface ErrorResponse {
+  details: string;
+}
+
+const tokenMissingError = 'Token is missing'
+
 const apiBackend = axios.create({
   baseURL: backendBaseUrl,
-  headers: { Authorization: localStorage.getItem(authToken) || '' },
+  headers: {
+    Authorization: localStorage.getItem(authToken) || '',
+  },
 });
+
+apiBackend.interceptors.request.use(
+  (config) => {
+    if (config.headers) config.headers.Authorization = localStorage.getItem(authToken) || '';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+apiBackend.interceptors.response.use(
+  (res) => {
+    console.log(res);
+    return res;
+  },
+  (error: AxiosError<ErrorResponse>): Promise<never> | undefined => {
+    const errorDetail = error.response?.data.details;
+    if (errorDetail === tokenMissingError) {
+      window.alert('다시 로그인을 진행해 주세요.')
+      window.history.pushState(null, "", '/');
+      window.location.reload();
+      return ;
+    }
+    return Promise.reject(error);
+  },
+);
 
 export const apiFetch = {
   get: <T>(url: string): Promise<T> => apiBackend.get(url).then((res) => res.data),
