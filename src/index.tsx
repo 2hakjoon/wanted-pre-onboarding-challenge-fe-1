36ac0b3310c22f7e67c90609e13b133118f632fa
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-15 22:25:34 +0900

    REFACTOR - split reactQuery file

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import { queryClient } from './reactQuery/reactQuery';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      useErrorBoundary: (error: any) => error.response?.status >= 500 || !error.response?.status,
-    },
-    mutations: {
-      useErrorBoundary: (error: any) => error.response?.status >= 500 || !error.response?.status,
-    },
-  },
-});
 
 root.render(
   <React.StrictMode>
