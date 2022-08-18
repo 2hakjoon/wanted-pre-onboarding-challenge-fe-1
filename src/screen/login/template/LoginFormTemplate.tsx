5af2e40214d4e86c8c534889c90408be635bb1d8
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-18 19:06:05 +0900

    REFACTOR - useLoginMutatin naming, useForm to custom hook

--- src/screen/login/template/LoginFormTemplate.tsx
+++ src/screen/login/template/LoginFormTemplate.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import { LoginError, LoginParams, LoginResponse } from '../../../api/Auth/types';
 import ButtonBasic from '../../../common/components/button/ButtonBasic';
 import InputLabel from '../../../common/components/input/InputLabel';
-import { emailPattern, passwordPattern } from '../../../common/constants/regex';
 import { authTokenKey, persistStore } from '../../../persistStore/persistStore';
-import useLogin from '../hooks/useLogin';
+import useLoginMutation from '../hooks/useLoginMutation';
+import useLoginForm from '../hooks/useLoginForm';
 
 export const LoginFormContainer = styled.form`
   border: 2px solid gray;
@@ -21,9 +20,8 @@ export const LoginFormContainer = styled.form`
 `;
 
 function LoginFormTemplate() {
-  const { register, getValues, formState, handleSubmit } = useForm<LoginParams>({ mode: 'onChange' });
-
-  const { mutate } = useLogin();
+  const { mutate } = useLoginMutation();
+  const { register, handleSubmit, isFormValid } = useLoginForm();
 
   const loginRequest = ({ email, password }: LoginParams) => {
     const onSuccess = ({ token }: LoginResponse) => {
@@ -39,21 +37,12 @@ function LoginFormTemplate() {
     mutate({ email, password }, { onSuccess, onError });
   };
 
-  const isNotValild = () => {
-    return (
-      Boolean(formState.errors.email?.type) === true ||
-      Boolean(formState.errors.password?.type) === true ||
-      !getValues('email') ||
-      !getValues('password')
-    );
-  };
-
   return (
     <LoginFormContainer onSubmit={handleSubmit(loginRequest)}>
       <span className="text-head">로그인</span>
       <InputLabel
         title="이메일"
-        register={register('email', { pattern: emailPattern })}
+        register={register('email')}
         data-cy="input-email"
         placeholder="이메일을 입력해주세요."
       />
@@ -61,10 +50,10 @@ function LoginFormTemplate() {
         title="비밀번호"
         data-cy="input-password"
         type="password"
-        register={register('password', { pattern: passwordPattern })}
+        register={register('password')}
         placeholder="비밀번호를 입력해주세요."
       />
-      <ButtonBasic title="로그인" type="submit" data-cy="button-login" disabled={isNotValild()} />
+      <ButtonBasic title="로그인" type="submit" data-cy="button-login" disabled={isFormValid()} />
       <a className="link-join" href="/join" data-cy="link-join">
         회원가입하기
       </a>
