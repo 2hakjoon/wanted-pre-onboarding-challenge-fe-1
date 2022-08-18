710df347fc18a611334e41410a2e86070e2ba41c
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-18 15:50:02 +0900

    CHANGE - hardcoded string to const variable and add export

--- src/screen/home/hooks/useGetTodos.ts
+++ src/screen/home/hooks/useGetTodos.ts
@@ -2,10 +2,10 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { apiTodos } from '../../../api/Todos/todos';
 import { ApiGetTodosResponse } from '../../../api/Todos/types';
 
-
+export const queryKeyGetTodos = ['Todo', 'getMany'] as const;
 
 function useGetTodos(options?: UseQueryOptions<ApiGetTodosResponse>) {
-  return useQuery<ApiGetTodosResponse>(['getTodos'], apiTodos.getTodos, options);
+  return useQuery<ApiGetTodosResponse>([...queryKeyGetTodos], apiTodos.getTodos, options);
 }
 
 export default useGetTodos;
