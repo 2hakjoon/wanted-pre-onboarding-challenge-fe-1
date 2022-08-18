d2334dc0d21ada9aa1e4e98b2da8046b9a848eb4
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-18 22:56:11 +0900

    REFACTOR - InputBasic compoenent. delete parent element

--- src/common/components/input/InputBasic.tsx
+++ src/common/components/input/InputBasic.tsx
@@ -1,36 +1,22 @@
-import React, { HTMLInputTypeAttribute } from 'react';
+import React from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+const Input = styled.input`
   width: 100%;
-  input {
-    width: 100%;
-    height: 30px;
-    border: 2px solid darkgray;
-    border-radius: 8px;
-    padding-left: 8px;
-  }
+  height: 30px;
+  border: 2px solid darkgray;
+  border-radius: 8px;
+  padding-left: 8px;
 `;
 
 interface InputBasicProps {
   register: UseFormRegisterReturn<string>;
   placeholder: string;
-  type?: HTMLInputTypeAttribute | undefined;
 }
 
-function InputBasic({ register, placeholder, type, ...rest }: InputBasicProps) {
-  return (
-    <Container {...rest}>
-      <input type={type} {...register} className="input" placeholder={placeholder} />
-    </Container>
-  );
+function InputBasic({ register, placeholder, ...rest }: InputBasicProps) {
+  return <Input {...register} className="input" placeholder={placeholder} {...rest} />;
 }
-
-InputBasic.defaultProps = {
-  type: undefined,
-};
 
 export default InputBasic;
