7c291a0c0859aeb4ce9010629d828cf4dd2e8076
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-18 19:01:48 +0900

    ADD - useLoginForm custom hook

---
+++ src/screen/login/hooks/useLoginForm.ts
@@ -0,0 +1,36 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { LoginParams } from '../../../api/Auth/types';
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+  })
+  .required();
+
+function useLoginForm() {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<LoginParams>({
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+  });
+
+  const isFormValid = () => {
+    return !!errors.email?.message || !!errors.password?.message;
+  };
+
+  useEffect(() => {
+    trigger();
+  }, []);
+
+  return { register, errors, handleSubmit, isFormValid };
+}
+
+export default useLoginForm;
